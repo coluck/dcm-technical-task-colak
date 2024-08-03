dbeb4b09a58fc2fb819c2f7e649d383ec2d0ae56
Oguz Colak <coluck@gmail.com>
2024-08-03 14:56:05 +0200

    Export  from  endpoint to adapt FE Upload dir select component

--- api/usecases.py
+++ api/usecases.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+
 from api.models import TestFilePath, TestEnvironment
 from api.serializers import TestFilePathSerializer, TestEnvironmentSerializer
 
@@ -5,5 +7,8 @@ from api.serializers import TestFilePathSerializer, TestEnvironmentSerializer
 def get_assets():
     return {
         'available_paths': TestFilePathSerializer(TestFilePath.objects.all().order_by('path'), many=True).data,
-        'test_envs': TestEnvironmentSerializer(TestEnvironment.objects.all().order_by('name'), many=True).data
+        'test_envs': TestEnvironmentSerializer(TestEnvironment.objects.all().order_by('name'), many=True).data,
+        'upload_dirs': settings.TEST_DIRS_WITHOUT_BASE_DIR,
+        # or it can be done like this if the source of truth is the database, but it's not the case here
+        # 'upload_dirs': {path.split('/')[0] for path in TestFilePath.objects.values_list('path', flat=True)},
     }
